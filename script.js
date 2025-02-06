d73ac64ffbf7781b52338fc5d7f035ac3420c8d1
abdulrehman929 <abdulrehmanadee@gmai.com>
2025-02-06 13:03:16 +0500

    Initial commit

--- script.js
+++ script.js
@@ -3,18 +3,32 @@ let input = document.querySelector('input');
 
 let String = "";
 let arr = Array.from(buttons);
-arr.forEach(button =>{
-    button.addEventListener('click',(e) =>{
-        if (e.target.innerHTML =='=') {
-            String= eval(String);
+
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            try {
+                String = eval(String);
+                input.value = String;
+                
+            } catch {
+                input.value = "Error";
+                String = "";
+            }
+        } else if (e.target.innerHTML == "AC") {
+            String = "";
             input.value = String;
-        }
-        else
-        String += e.target.innerHTML== "AC";
-        input.value= String="";
+            
+        } else if (e.target.innerHTML == "Del") {
+            String = String.slice(0 , -1);
+            input.value = String;
+            
+        } 
 
-
-        
-    })
-})
- 
+        else {
+            String += e.target.innerHTML;
+            input.value = String;
+        } 
+    });
+   
+});
